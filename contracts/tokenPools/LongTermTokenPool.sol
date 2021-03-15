52a1340552173475315deb6d0ea29bd9a26d6e15
madjarevicn <nikola.madjarevic@gmail.com>
2021-03-15 13:45:55 +0100

    Add utils.js to save deployed contracts to contract-addresses.json file.

---
+++ contracts/tokenPools/LongTermTokenPool.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.2;
+
+/**
+ * LongTermTokenPool contract.
+ * @author Nikola Madjarevic
+ * Date created: 15.3.21.
+ * Github: madjarevicn
+ */
+contract LongTermTokenPool {
+
+}
