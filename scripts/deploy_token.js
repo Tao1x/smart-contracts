acf6d967cccf6fc19cb606fdc6ae0d31fe1c4bff
madjarevicn <nikola.madjarevic@gmail.com>
2021-03-15 13:48:32 +0100

    Add functions for saving contract addresses and getting saved contract addresses.

--- scripts/deploy_token.js
+++ scripts/deploy_token.js
@@ -1,10 +1,6 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 const { toHordDenomination } = require('../test/setup');
+const { getSavedContractAddresses, saveContractAddress } = require('./utils')
 
 async function main() {
 
@@ -12,15 +8,11 @@ async function main() {
 
   // We get the contract to deploy
   const Hord = await hre.ethers.getContractFactory("Hord");
-  const hord = await Hord.deploy(
-      "HORD token",
-      "HORD",
-      toHordDenomination(320000000)
-  );
-
+  const hord = await Hord.deploy("HORD token", "HORD", toHordDenomination(320000000));
   await hord.deployed();
 
   console.log("Hord token deployed to:", hord.address);
+  saveContractAddress(hre.network.name, 'hordToken', hord.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
