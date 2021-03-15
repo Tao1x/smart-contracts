0a0e00c1559d9e33a7d4339a92ca5bd677f63542
madjarevicn <nikola.madjarevic@gmail.com>
2021-03-15 13:28:57 +0100

    Rename sample-script.js to deploy_token.js

--- scripts/deploy_token.js
+++ scripts/deploy_token.js
@@ -1,25 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { toHordDenomination } = require('../test/setup');
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile 
-  // manually to make sure everything is compiled
+
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Hord = await hre.ethers.getContractFactory("Hord");
+  const hord = await Hord.deploy(
+      "HORD token",
+      "HORD",
+      toHordDenomination(320000000)
+  );
 
-  await greeter.deployed();
+  await hord.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Hord token deployed to:", hord.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
