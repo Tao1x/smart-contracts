235f5f174567eb93cc9d985f74b45292307ea432
madjarevicn <nikola.madjarevic@gmail.com>
2021-03-16 08:05:37 +0100

    Add tenderly_push.js script

--- contracts/tokenPools/LongTermTokenPool.sol
+++ contracts/tokenPools/LongTermTokenPool.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.8.2;
 
+import "@openzeppelin/upgrades-core/contracts/Initializable.sol";
+
 /**
  * LongTermTokenPool contract.
  * @author Nikola Madjarevic
  * Date created: 15.3.21.
  * Github: madjarevicn
  */
-contract LongTermTokenPool {
+contract LongTermTokenPool is Initializable {
 
+    function initialize()
+    public
+    initializer
+    {
+
+    }
 }
