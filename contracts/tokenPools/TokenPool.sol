a84921d54c7af0c78889a07f55f2636d11270b5e
madjarevicn <nikola.madjarevic@gmail.com>
2021-03-16 11:21:55 +0100

    Create abstract TokenPool.sol contract.

---
+++ contracts/tokenPools/TokenPool.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/proxy/Initializable.sol";
+import "../interfaces/IERC20.sol";
+
+/**
+ * LongTermTokenPool contract.
+ * @author Nikola Madjarevic
+ * Date created: 15.3.21.
+ * Github: madjarevicn
+ */
+abstract contract TokenPool is Initializable {
+
+    uint256 public initialSupply;
+    IERC20 public token;
+
+    function initialize(
+        address _token,
+        uint _initialSupply
+    )
+    public
+    initializer
+    {
+        token = IERC20(_token);
+        initialSupply = _initialSupply;
+    }
+}
