59e904d7aafc6341dcc33e611632b95edc595513
madjarevicn <nikola.madjarevic@gmail.com>
2021-03-31 08:22:57 +0200

    Remove sufficient sub256 function.

--- contracts/governance/HordCongress.sol
+++ contracts/governance/HordCongress.sol
@@ -240,11 +240,6 @@ contract HordCongress {
         return c;
     }
 
-    function sub256(uint256 a, uint256 b) internal pure returns (uint) {
-        require(b <= a, "subtraction underflow");
-        return a - b;
-    }
-
     receive() external payable {
         emit ReceivedEther(msg.sender, msg.value);
     }
