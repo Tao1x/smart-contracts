171c053bab85c0903e91e5e61d393c78b3381dc2
madjarevicn <nikola.madjarevic@gmail.com>
2021-05-08 10:22:44 +0200

    Add proper MaintainersRegistry.sol contract along with interface.

---
+++ contracts/interfaces/IMaintainersRegistry.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.12;
+
+/**
+ * IMaintainersRegistry contract.
+ * @author Nikola Madjarevic
+ * Date created: 8.5.21.
+ * Github: madjarevicn
+ */
+interface IMaintainersRegistry {
+    function isMaintainer(address _address) external view returns (bool);
+}
--- contracts/system/MaintainersRegistry.sol
+++ contracts/system/MaintainersRegistry.sol
@@ -1,100 +1,120 @@
-//"SPDX-License-Identifier: UNLICENSED"
+// "SPDX-License-Identifier: UNLICENSED"
 pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/proxy/Initializable.sol";
+import "../libraries/SafeMath.sol";
 
-/**
- * MaintainersRegistry contract.
- * @author Nikola Madjarevic
- * Date created: 18.3.21.
- * Github: madjarevicn
- */
 contract MaintainersRegistry is Initializable {
 
-    // Address of HordCongress contract
-    address hordCongress;
-    // Array holding addresses of all maintainers
-    address [] maintainers;
-    // Mapping representing if specific address is a maintainer
-    mapping(address => bool) isMaintainer;
+    using SafeMath for uint;
 
-    // Modifier restricting calls only to HordCongress
-    modifier onlyHordCongress {
-        require(msg.sender == hordCongress);
-        _;
-    }
+    // Mappings
+    mapping(address => bool) _isMaintainer;
+
+
+    // Arrays
+    address [] public allMaintainers;
+
+    // chainport congress authorized address to modify maintainers
+    address public hordCongress;
+
+    // Events
+    event MaintainerStatusChanged(address maintainer, bool isMember);
 
     /**
-     * @notice  Function to set maintainers initially
-     * @param   _maintainers is the array of initial maintainer addresses
-     * @param   _hordCongress is the address of HordCongress contract
+     * @notice      Function to perform initialization
      */
-    function initialize(address [] memory _maintainers, address _hordCongress) public initializer {
-        for(uint i = 0; i < _maintainers.length; i++) {
-            address _maintainer = _maintainers[i];
-            maintainers.push(_maintainer);
-            isMaintainer[_maintainer] = true;
-        }
-
-        // Set the congress address
+    function initialize(
+        address [] memory _maintainers,
+        address _hordCongress
+    )
+    public
+    initializer
+    {
+        // Register congress
         hordCongress = _hordCongress;
-    }
 
-    /**
-     * @notice  Function to add array of maintainers
-     * @param   _maintainers is the array of maintainers to add
-     */
-    function addMaintainers(address [] memory _maintainers) public onlyHordCongress {
-        for(uint i=0; i < _maintainers.length; i++) {
-            address _maintainer = _maintainers[i];
-            // Skip if maintainer is already present
-            if(!isMaintainer[_maintainer]) {
-                maintainers.push(_maintainers[i]);
-                isMaintainer[_maintainers[i]] = true;
-            }
-        }
-    }
-
-    /**
-     * @notice  Function to remove array of maintainers
-     * @param   _maintainers is the array of maintainers to remove
-     */
-    function removeMaintainers(address [] memory _maintainers) public onlyHordCongress {
         for(uint i = 0; i < _maintainers.length; i++) {
-            removeMaintainer(_maintainers[i]);
+            addMaintainerInternal(_maintainers[i]);
         }
     }
 
+    function addMaintainer(
+        address _address
+    )
+    public
+    {
+        require(msg.sender == hordCongress, 'MaintainersRegistry :: Only congress can add maintainer');
+        addMaintainerInternal(_address);
+    }
+
     /**
-     * @dev Internal handling removing maintainers
+     * @notice      Function that serves for adding maintainer
+     * @param       _address is the address that we want to give maintainer privileges to
      */
-    function removeMaintainer(address _maintainer) internal {
-        // Maintainer has to exist
-        require(isMaintainer[_maintainer] == true);
+    function addMaintainerInternal(
+        address _address
+    )
+    internal
+    {
+        require(_isMaintainer[_address] == false);
+
+        // Adds new maintainer to an array
+        allMaintainers.push(_address);
+        // Sets that address is now maintainer
+        _isMaintainer[_address] = true;
+
+        // Emits event for change of address status to maintainer
+        emit MaintainerStatusChanged(_address, true);
+    }
+
+    /**
+     * @notice      Function that serves for removing maintainer
+     * @param       _maintainer is target address of the maintainer we want to remove
+     */
+    function removeMaintainer(
+        address _maintainer
+    )
+    external
+    {
+        require(msg.sender == hordCongress, 'MaintainersRegistry :: Only congress can remove maintainer');
+        require(_isMaintainer[_maintainer] == true);
+
+        uint length = allMaintainers.length;
+
         uint i = 0;
-        // Find the index position of the maintainer
-        while(maintainers[i] != _maintainer) {
-            if(i == maintainers.length) {
-                revert("Passed maintainer address does not exist");
+
+        // While loop for finding position of targeted address
+        while(allMaintainers[i] != _maintainer){
+            if(i == length){
+                revert();
             }
+            i++;
         }
-        // Copy the last admin position to the current index
-        maintainers[i] = maintainers[maintainers.length-1];
-        // Change maintainer status
-        isMaintainer[_maintainer] = false;
-        // Remove the last admin, since it's double present
-        maintainers.pop();
+
+        // Removes address from maintainers array
+        allMaintainers[i] = allMaintainers[length - 1];
+
+        // Removes last element from an array
+        allMaintainers.pop();
+
+        // Sets that address is no longer maintainer
+        _isMaintainer[_maintainer] = false;
+
+        // Emits event for change of address status to non-maintainer
+        emit MaintainerStatusChanged(_maintainer, false);
     }
 
+
     /**
-     * @notice  Function to get all maintainers, no need for allowing query by start and end index
-     *          since array of maintainers will be controlled and never will exceed block gas limit.
+     * @notice      Function to check if wallet is maintainer
+     * @param       _address is the wallet to check
      */
-    function getMaintainers()
+    function isMaintainer(address _address)
     external
     view
-    returns (address [] memory)
+    returns (bool)
     {
-        return maintainers;
+        return _isMaintainer[_address];
     }
 }
